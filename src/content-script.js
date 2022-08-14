a639503a990c59d9b61828e6fe73ca44111d8540
Mikhail Loginov <rscience.code@gmail.com>
2022-08-14 12:32:47 +0300

    Add fileSaver to download files directly
    - Instead of opening a tab save files directly
    - Add basic webpack config

--- src/content-script.js
+++ src/content-script.js
@@ -1,3 +1,5 @@
+import { saveAs } from 'file-saver';
+
 (function () {
   if (window.hasRun) {
     return;
@@ -6,10 +8,10 @@
   window.hasRun = true;
 
   function getAudio() {
-    console.log('hello');
     const url = document.querySelector('.us audio source[type="audio/mpeg"]').src;
 
-    window.open(url, '_blank');
+    const filename = url.split('/').pop();
+    saveAs(url, filename);
   }
 
   browser.runtime.onMessage.addListener(getAudio);
